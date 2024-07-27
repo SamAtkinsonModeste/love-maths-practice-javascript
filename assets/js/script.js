c9eff70429a4b3588e78978b4de5c9bfd951c169
Samantha Atkinson <sammodeste@yahoo.com>
2024-07-27 01:08:47 +0100

    js:runGame, doc listener and displayAdditionQuestion func

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("You clicked Submit!");
           } else {
             let gameType = this.getAttribute('data-type');
-            alert(`You clicked ${gameType}`);
+            runGame(gameType);
           }
       });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -27,11 +29,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     //STEPS:Create two random numbers between 1 and 25
     //NOTE: The reason why we add the plus one is so we do not get zero as a number
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+        if (gameType === "addition") {
+            displayAdditionQuestion(num1, num2);
+        } else {
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknown game type: ${gameType}. Aborting!`;
+        }
 }
 
 
@@ -50,8 +59,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
